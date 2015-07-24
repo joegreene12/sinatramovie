fd16097c604e57b2655cdba32c711a099c20ecc5
Joseph Greene <joegreene12@github.com>
2015-07-24 15:04:52 -0500

    maybe

--- app.rb
+++ app.rb
@@ -1,2 +1,12 @@
 require 'bundler'
 Bundler.require()
+
+
+def fake_movie(name, release_year, description)
+{:name => name.to_s, :release_year => release_year.to_s, :description => description.to_s}.to_json
+end
+
+get '/' do
+  cross_origin
+  fake_movie{'lazerCats''2020','Anyone who wants to see cats with lazers. what can get better than that!'}
+end
